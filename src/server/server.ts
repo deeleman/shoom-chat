0ebce8633e909ce19b9f73dc00576b56d67e4f7f
Pablo Deeleman <deeleman@gmail.com>
2025-05-31 00:14:19 +0200

    chore: Better structure for bundle output dir and server initialization

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,12 @@
-import { createServer } from 'node:http';
 import * as path from 'node:path';
+import { createServer } from 'node:http';
 import express from 'express';
 import type { Request, Response } from 'express';
 import { Server } from 'socket.io';
-import { UserService } from './services/UserService.js';
-import { SocketService } from './services/SocketService.js';
+import { UserService, SocketService } from './services';
 
 // Configure build file path and filename location
-const BUILD_PATH = '../../dist';
+const BUILD_PATH = '../../dist/client';
 const INDEX_FILENAME = 'index.html';
 const { pathname: __dirname } = new URL('../server', import.meta.url);
 
@@ -29,16 +28,15 @@ app.use(express.static(path.join(__dirname, BUILD_PATH)));
 if (process.env.NODE_ENV === 'production') {
 }
 
-// Health API endpoint
+// Expose a Health API endpoint for the CI pipeline
 app.get("/hello", (_: Request, res: Response) => {
   res.send("Hello Soom Chat!");
 });
 
-// Fallback catch-all route that serves React app's index.html regardless clientside route.
+// Fallback catch-all route that serves React app's index.html
 app.get('*channel', (_: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, BUILD_PATH, INDEX_FILENAME));
 });
-
 
 // Start the server
 httpServer.listen(PORT, () => {
@@ -46,8 +44,9 @@ httpServer.listen(PORT, () => {
   console.log(`Socket.IO listening for connections.`);
 });
 
-// Instantiate and initialize services
-const userService = new UserService();
-SocketService.initialize(httpServer, userService, () => {
-  console.log('Web Sockets server is running');
+// Instantiate and initialize data services
+SocketService.initialize({
+  httpServer,
+  userService: new UserService(),
+  onReady: () => console.log('Web Sockets server is running'),
 });
