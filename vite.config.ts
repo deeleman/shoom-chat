94f0e526373caa403ddba11b3352651e601b76cd
Pablo Deeleman <deeleman@gmail.com>
2025-05-31 12:37:13 +0200

    feat: Better filesystem design and encapsulation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src/client',
   build: {
-    outDir: './dist/client',
+    outDir: '../../dist/client',
     emptyOutDir: true,
-  }
+  },
 });
